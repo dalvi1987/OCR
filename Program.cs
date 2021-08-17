10ea1e39324cd34518e54c141006bc9a9c152b42
dalvi.shekhar <dalvi.shekhar@gmail.com>
2021-08-17 10:53:31 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using Tesseract;
+
+namespace OCR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            doOCR();
+            Console.ReadLine();
+
+        }
+
+        public static void doOCR()
+        {
+            using (var engine = new TesseractEngine(@"C:\Users\Shekhar\source\repos\OCR\tessdata", "eng", EngineMode.Default))
+            {
+                using (var img = Pix.LoadFromFile(@"C:\Shekhar\Passport_1.PNG"))
+                {
+                    var page = engine.Process(img);
+                    var result = page.GetText();
+                    Console.WriteLine(result);
+                }
+            }
+        }
+    }
+}
